d2cbf6877dce1e7bec48eaae8a394a0d5e953553
PRADHUMAN KUMAR <121790103+pk4727@users.noreply.github.com>
2023-08-01 00:30:18 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -12,7 +12,7 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('Diabetes_prediction_trained_model.sav', 'rb'))
 
 heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
